0fb675baa96a2f751cfe06f1eb9f190fc9479810
Bharat Chauhan <tell.bhch@gmail.com>
2022-09-11 14:13:25 +0530

    Accept more args in wiget: validate_on_submit and attrs

--- django_jsonform/widgets.py
+++ django_jsonform/widgets.py
@@ -10,12 +10,21 @@ from django.urls import reverse, NoReverseMatch
 class JSONFormWidget(forms.Widget):
     template_name = 'django_jsonform/editor.html'
 
-    def __init__(self, schema, model_name='', file_handler=''):
-        super().__init__()
+    def __init__(
+        self,
+        schema,
+        model_name='',
+        file_handler='',
+        validate_on_submit=False,
+        attrs=None,
+    ):
+        super().__init__(attrs=attrs)
 
         self.schema = schema
         self.model_name = model_name
         self.file_handler = file_handler
+        self.validate_on_submit = validate_on_submit
+
 
     def render(self, name, value, attrs=None, renderer=None):
         if callable(self.schema):
@@ -28,19 +37,21 @@ class JSONFormWidget(forms.Widget):
 
         schema = normalize_schema(schema)
 
-        context = {
-            'name': name,
+        context = self.get_context(name, value, attrs)
+
+        context['widget'].update({
             'model_name': self.model_name,
             'data': value or json.dumps(''),
             'schema': json.dumps(schema),
             'file_handler': self.file_handler or get_setting('FILE_HANDLER', ''),
-            'error_map': getattr(self, 'error_map', {})
-        }
+            'error_map': getattr(self, 'error_map', {}),
+            'validate_on_submit': self.validate_on_submit
+        })
 
         # backwards compatibility for `JSONFORM_UPLOAD_HANDLER` setting
-        if not context['file_handler']:
+        if not context['widget']['file_handler']:
             try:
-                context['file_handler'] = reverse('django_jsonform:upload')
+                context['widget']['file_handler'] = reverse('django_jsonform:upload')
             except NoReverseMatch:
                 pass
 
