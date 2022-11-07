ac574a29f00dbd5a769bccd04cf0e0e328a85af1
Gary Zheng <gary@arcana.io>
2022-11-07 14:43:23 -0500

    copy paste base functions

--- django_jsonform/contrib/dataclasses/models/fields.py
+++ django_jsonform/contrib/dataclasses/models/fields.py
@@ -16,3 +16,41 @@ class DataclassJSONField(JSONField):
         )
 
         super().__init__(schema=self._json_schema, **kwargs)
+
+
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+        # Some backends (SQLite at least) extract non-string values in their
+        # SQL datatypes.
+        if isinstance(expression, KeyTransform) and not isinstance(value, str):
+            return value
+        try:
+            return json.loads(value, cls=self.decoder)
+        except json.JSONDecodeError:
+            return value
+
+    def get_internal_type(self):
+        return "JSONField"
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+        return json.dumps(value, cls=self.encoder)
+
+    def get_transform(self, name):
+        transform = super().get_transform(name)
+        if transform:
+            return transform
+        return KeyTransformFactory(name)
+
+    def validate(self, value, model_instance):
+        super().validate(value, model_instance)
+        try:
+            json.dumps(value, cls=self.encoder)
+        except TypeError:
+            raise exceptions.ValidationError(
+                self.error_messages["invalid"],
+                code="invalid",
+                params={"value": value},
+            )
