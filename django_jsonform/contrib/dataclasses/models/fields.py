d8471d743050913ed149d6f1d4e3d63dce42be09
Gary Zheng <gary@arcana.io>
2022-11-07 14:29:54 -0500

    basic DataclassJSONField without serdes

--- django_jsonform/contrib/dataclasses/models/fields.py
+++ django_jsonform/contrib/dataclasses/models/fields.py
@@ -1,6 +1,20 @@
+from typing import Any
+from typing import Type
+
+from dataclasses_jsonschema import JsonSchemaMixin
+from dataclasses_jsonschema.type_defs import JsonDict
+from django_jsonform.contrib.dataclasses.models.utils import json_schema_array
+
+from django_jsonform.models.fields import JSONField
 
 
 
 
-class Foo:
-    pass
+
+class DataclassJSONField(JSONField):
+    def __init__(self, dataclass_cls:Type[JsonSchemaMixin],*,many:bool=False, **kwargs:Any)->None:
+        self._dataclass_cls=dataclass_cls
+        self._many=many
+        self._json_schema=json_schema_array(dataclass_cls) if many else dataclass_cls.json_schema()
+
+        super().__init__(schema=self._json_schema, **kwargs)
---
+++ django_jsonform/contrib/dataclasses/models/utils.py
@@ -0,0 +1,22 @@
+import dataclasses
+from typing import Type
+
+from dataclasses_jsonschema import JsonSchemaMixin
+from dataclasses_jsonschema.type_defs import JsonDict
+
+
+def json_schema_array(dataclass_cls: Type[JsonSchemaMixin]) -> JsonDict:
+    """Get JSON schema representing an array of `dataclass_cls`."""
+    WrapperDataClass = type("WrapperDataClass", (JsonSchemaMixin,), {})
+    WrapperDataClass.__annotations__["item"] = dataclass_cls
+    WrapperDataClass = dataclasses.dataclass(WrapperDataClass)
+
+    schema: JsonDict = WrapperDataClass.json_schema()  # type: ignore
+
+    schema["type"] = "array"
+    schema["description"] = f"An array of {dataclass_cls.__name__} objects."
+    del schema["required"]
+    schema["items"] = schema["properties"]["item"]
+    del schema["properties"]
+
+    return schema
