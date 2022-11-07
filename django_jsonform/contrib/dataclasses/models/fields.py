58159b5b0868adf4260773a033a9636484b29abd
Gary Zheng <gary@arcana.io>
2022-11-07 14:31:51 -0500

    lint

--- django_jsonform/contrib/dataclasses/models/fields.py
+++ django_jsonform/contrib/dataclasses/models/fields.py
@@ -1,20 +1,18 @@
-from typing import Any
-from typing import Type
+from typing import Any, Type
 
 from dataclasses_jsonschema import JsonSchemaMixin
-from dataclasses_jsonschema.type_defs import JsonDict
 from django_jsonform.contrib.dataclasses.models.utils import json_schema_array
-
 from django_jsonform.models.fields import JSONField
 
 
-
-
-
 class DataclassJSONField(JSONField):
-    def __init__(self, dataclass_cls:Type[JsonSchemaMixin],*,many:bool=False, **kwargs:Any)->None:
-        self._dataclass_cls=dataclass_cls
-        self._many=many
-        self._json_schema=json_schema_array(dataclass_cls) if many else dataclass_cls.json_schema()
+    def __init__(
+        self, dataclass_cls: Type[JsonSchemaMixin], *, many: bool = False, **kwargs: Any
+    ) -> None:
+        self._dataclass_cls = dataclass_cls
+        self._many = many
+        self._json_schema = (
+            json_schema_array(dataclass_cls) if many else dataclass_cls.json_schema()
+        )
 
         super().__init__(schema=self._json_schema, **kwargs)
