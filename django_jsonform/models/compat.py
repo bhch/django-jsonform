bf78cd86dd9135477fef12014f5a344d008d65cd
Bharat Chauhan <tell.bhch@gmail.com>
2022-11-17 12:39:04 +0530

    Fix #73: Remove redundant deconstruct  method

--- django_jsonform/models/compat.py
+++ django_jsonform/models/compat.py
@@ -51,12 +51,6 @@ class JSONField(models.TextField):
 
     def deconstruct(self):
         name, path, args, kwargs = super().deconstruct()
-        kwargs['encoder'] = self.encoder
-        kwargs['decoder'] = self.decoder
-        return name, path, args, kwargs
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
         if self.encoder is not None:
             kwargs['encoder'] = self.encoder
         if self.decoder is not None:
