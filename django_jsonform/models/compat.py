051d224b70c3f4d3fcd37bb8ff904456ffd14d82
Nicolas Forstner <70640646+nlsfnr@users.noreply.github.com>
2024-01-09 17:33:00 +0000

    Add missing import, initialize missing variable (PR #132)
    - The exceptions module was used but not imported.
    - The next_validator variable was not initialized in a certain code path, resulting in UnboundLocalErrors
    
    ---------
    
    Co-authored-by: Bharat Chauhan <tell.bhch@gmail.com>

--- django_jsonform/models/compat.py
+++ django_jsonform/models/compat.py
@@ -33,6 +33,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import json
 from django.db import models
+from django.core import exceptions
 from django_jsonform.forms.fields import JSONFormField
 
 
@@ -95,4 +96,3 @@ class JSONField(models.TextField):
             'decoder': self.decoder,
             **kwargs,
         })
-
